a555783cbb4202c95b8aca9ff88a0a110f232afc
VU Alex <alex.vu@etu.univ-cotedazur.fr>
2021-03-18 18:12:52 +0100

    correction du commit précédent

---
+++ app/src/main/java/edu/iut/m414/distimate/GameActivity.java
@@ -0,0 +1,13 @@
+package edu.iut.m414.distimate;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
+}
