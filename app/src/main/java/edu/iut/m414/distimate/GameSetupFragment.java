ea67bf436daa618b65986ad3c18374c1f47773b5
DannyVuIUT <danny.vu1@etu.univ-cotedazur.fr>
2021-03-19 20:10:40 +0100

    Ajout de l'affichage et de l'implémentation du GameSetupFragment

--- app/src/main/java/edu/iut/m414/distimate/GameSetupFragment.java
+++ app/src/main/java/edu/iut/m414/distimate/GameSetupFragment.java
@@ -1,16 +1,23 @@
 package edu.iut.m414.distimate;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import edu.iut.m414.distimate.util.GameLoadingStateListener;
+import edu.iut.m414.distimate.util.GameStartListener;
 
 public class GameSetupFragment extends Fragment implements GameLoadingStateListener {
+    GameStartListener startListener;
+    Button startButton;
+
     public GameSetupFragment() {
         // Required empty public constructor
     }
@@ -20,12 +27,22 @@ public class GameSetupFragment extends Fragment implements GameLoadingStateListe
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_game_setup, container, false);
 
-        // TODO : récupérer les textes et boutons pour changer l'état de chargement
+        startButton = rootView.findViewById(R.id.startButton);
+        startButton.setOnClickListener((view) -> {
+            startListener.onGameStart();
+        });
+        startButton.setClickable(false);
         return rootView;
     }
 
     @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.startListener = (GameStartListener) getActivity();
+    }
+
+    @Override
     public void onGameLoaded() {
-        // TODO : changer l'état des boutons
+        startButton.setClickable(true);
     }
 }
